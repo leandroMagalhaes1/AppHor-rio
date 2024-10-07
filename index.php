d7b1016fd3256f9cb0b4b3e0d664f6bee520d9eb
leandroMagalhaes1 <146432380+leandroMagalhaes1@users.noreply.github.com>
2024-10-07 20:09:52 +0100

    Connected with XAMPP, started using php!

--- index.php
+++ index.php
@@ -1,15 +1,22 @@
+<?php 
+
+    define('NAME','Leandro');
+?>
+
 <!DOCTYPE html>
 <html>
 
 <head>
-
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>app</title>
 
 </head>
 
 <body>
-
-    <h1>Adiciona gasto</h1>
+    <h1><?php echo "Bem vindo ". NAME; ?></h1>
+    <h2>Adiciona gasto</h2>
 
     <div id="div_text" style="display: flex; align-items: center;">
         <label for="i_text">Gasto:</label>
